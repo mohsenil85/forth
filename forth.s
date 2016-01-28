3820f5395aaf1edc6f9f02b8e5116195a6dc9b48
mohsenil85 <mohsenil85@gmail.com>
2016-01-28 12:55:00 -0800

    init

---
+++ forth.s
@@ -0,0 +1,29 @@
+            ;; forth.s
+            ;; Wed Jan 27 18:47:05 PST 2016
+            ;; should all be in 32bit intel?
+
+%macro NEXT 0
+    lodsw
+    jmp [eax]
+%endmacro
+
+        global start
+
+start: 
+        push dword len
+        push dword msg
+        push dword 1            ;stdout
+        mov eax, 4
+        sub esp, 4
+        int 0x80
+        add esp, 16
+        
+        push dword 0
+        mov eax, 1
+        sub esp, 12
+        int 0x80
+        
+section .data
+msg:    db      "Hello world!", 10
+len:    equ $ - msg
+        
---
+++ run.sh
@@ -0,0 +1,1 @@
+nasm -g -f macho forth.s && ld -macosx_version_min 10.7.0 forth.o && ./a.out
