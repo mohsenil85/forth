ee1300edec48445442a810b2b28287fd9f4d9fc5
logan <mohsenil85@gmail.com>
2016-01-29 03:50:36 +0000

    add comments, DOCOL

--- forth.s
+++ forth.s
@@ -1,6 +1,6 @@
-            ;; forth.s
-            ;; Wed Jan 27 18:47:05 PST 2016
-            ;; should all be in 32bit intel?
+	;; forth.s
+	;; Wed Jan 27 18:47:05 PST 2016
+	;; should all be in 32bit intel?
 
 	%macro NEXT 0
 	lodsw
@@ -8,31 +8,37 @@
 	%endm
 
 	%macro PUSHRSP 1
-	lea ebp, [ebp-4]
-	mov ebp, %1
+	lea ebp, [ebp-4]	;make space on the ebp stack
+	mov ebp, %1		;store whats in ARG on there
 	%endm
 
 	%macro POPRSP 1
-	mov %1,ebp
-	lea ebp, [ebp+4]
+	mov %1,ebp		;pop whats on ebp, return to REG
+	lea ebp, [ebp+4]	;move stack pointer "up" or "back"
 	%endm
 
+
 section .data
+
 msg:    db      "Hennnnnnrld!",0x0a
 len:	equ 	$ - msg
         
 
 section .text
 
-global 	_start
+DOCOL:
+	PUSHRSP esi
+	add eax, 4
+	mov esi, eax
 
+global 	_start
 _start:
 	mov eax, 1
 	mov ebx, 2
 	mov ecx, 3
 	mov edx, 4
-	POPRSP eax
 	PUSHRSP edx
+	POPRSP eax
 	;NEXT
 	;do writing
 	mov edx, len	;load length
