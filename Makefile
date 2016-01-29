8330004a0529effea6bbdfc2289a0ea9d4812534
logan <mohsenil85@gmail.com>
2016-01-29 02:51:53 +0000

    add text section to executable

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all: forth.o
-	ld -m elf_i386 -g forth.o
+	ld -m elf_i386 forth.o
 
 forth.o: forth.s
-	nasm -f elf32 -g -F stabs forth.s
+	nasm -f elf -F dwarf -g forth.s
 
 clean:
 	rm forth.o a.out
--- forth.s
+++ forth.s
@@ -2,29 +2,41 @@
             ;; Wed Jan 27 18:47:05 PST 2016
             ;; should all be in 32bit intel?
 
-%macro NEXT 0
-    lodsw
-    jmp [eax]
-%endmacro
+	%macro NEXT 0
+	lodsw
+	jmp [eax]
+	%endm
 
-        global _start
+	%macro PUSHRSP 1
+	lea ebp, [ebp-4]
+	mov $1, [ebp]
+	%endmacro
 
-_start: 
+section .data
+msg:    db      "Hennnnnnrld!",0x0a
+len:	equ 	$ - msg
+        
+
+section .text
+
+global 	_start
+
+_start:
+	mov eax, 1
+	mov ebx, 1
+	mov ecx, 1
+	mov edx, 1
+	;NEXT
 	;do writing
-	mov edx, len	;stdout
-	mov ecx, msg	;stdout
+	mov edx, len	;load length
+	mov ecx, msg	;load string
 	mov ebx, 1	;stdout
 	mov eax, 4	;syscall for write
 	int 0x80	;call kernel
+
 	
 	;exit
 	mov ebx, 0 ;exit status
 	mov eax, 1 ;syscall for exit
+	int 0x80	;call kernel
 	
-	
-
-        
-section .data
-msg:    db      "Hello world!", 10
-len:    equ $ - msg
-        
