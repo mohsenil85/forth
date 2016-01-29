67221bb9dcb331f0a07511466dd0f525e3de12df
logan <mohsenil85@gmail.com>
2016-01-29 05:45:03 +0000

    add more macros

--- forth.s
+++ forth.s
@@ -3,7 +3,7 @@
 	;; should all be in 32bit intel?
 
 	%macro NEXT 0
-	lodsw
+	lodsd
 	jmp [eax]
 	%endm
 
@@ -17,14 +17,39 @@
 	lea ebp, [ebp+4]	;move stack pointer "up" or "back"
 	%endm
 
+;	%macro defcode 4,0 ;name,namelen,label,flags=0
+;	section .rodata
+;	align 4
+;	global %1_%3 ;ie, name_label
+;%1_%3:
+	
+
+
 
 section .data
-
-msg:    db      "Hennnnnnrld!",0x0a
-len:	equ 	$ - msg
-        
+	msg:    db      "Hennnnnnrld!",0x0a
+	len:	equ 	$ - msg
 
 section .text
+	align 4
+
+;;;;my stuff
+kernel:
+	int 0x80	;call kernel
+	
+write_thing:
+	mov edx, len	;load length
+	mov ecx, msg	;load string
+	mov ebx, 1	;stdout
+	mov eax, 4	;syscall for write
+	call kernel
+
+bye:
+	mov ebx, 0 	;exit status
+	mov eax, 1 	;syscall for exit
+	call kernel
+
+;;;;jonestuff
 
 DOCOL:
 	PUSHRSP esi
@@ -33,23 +58,22 @@ DOCOL:
 
 global 	_start
 _start:
+	cld 		;clear direction flag (?)
+	mov esp, cold_start
+	lodsd
+	jmp [eax]
+	;NEXT
+	
+section .rodata
+
+cold_start:
 	mov eax, 1
 	mov ebx, 2
 	mov ecx, 3
 	mov edx, 4
-	PUSHRSP edx
-	POPRSP eax
-	;NEXT
-	;do writing
-	mov edx, len	;load length
-	mov ecx, msg	;load string
-	mov ebx, 1	;stdout
-	mov eax, 4	;syscall for write
-	int 0x80	;call kernel
-
 	
-	;exit
-	mov ebx, 0 ;exit status
-	mov eax, 1 ;syscall for exit
-	int 0x80	;call kernel
+	call write_thing
+
+	call bye	
+	
 	
